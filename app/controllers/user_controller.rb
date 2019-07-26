6d3412802edaafc7cd05eac477bb419b737c3f78
lilleonz <lilleonz@gmail.com>
2019-07-26 19:23:23 -0400

    udated user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,20 @@
 class UserController < ApplicationController
-    def new
+  def new
+  end
+
+  def create
+    # @user = User.new(user_params)
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
-    
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to new_user_path
-        else
-            render :new
-        end
-    end
-    private
-    def user_params
-        params.require(:user).permit(:username,:email,:password)
-    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
 end
