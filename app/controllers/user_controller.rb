87a2d8aacf6f0f7a69d48676cdcfd824df04912f
eapenzacharias <eapenzac@gmail.com>
2019-07-30 01:47:41 +0200

    edit and update added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,6 +13,20 @@ class UserController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if 
+      @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
