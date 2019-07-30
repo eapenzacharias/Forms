19177b446e9e7d7d882d76103687ed2888ad7452
lilleonz <lilleonz@gmail.com>
2019-07-30 11:59:55 -0400

    removed commented code from user controller line 8

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,6 @@ class UserController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    #@user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
     if @user.save
       redirect_to new_user_path
     else
